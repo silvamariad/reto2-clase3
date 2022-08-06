76a9b90e29a16fc70af94d43e15e0b7953662f08
Maria Silva <m.angelessilvagutierrez@gmail.com>
2022-08-05 23:49:47 -0500

    crear la carpeta foo

---
+++ foo/automatizacion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#creo las carpetas en reto1
+
+mkdir foo
+mkdir foo/dummy
+mkdir foo/empty
+
+
+
---
+++ foo/automover.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#creo las carpetas en reto1
+
+#mkdir foo
+#mkdir foo/dummy
+#mkdir foo/empty
+
+mv file2.txt -t ./foo/empty
+
+
