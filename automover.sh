249bd543188e539b9555f3a5d76617faf46244c9
Maria Silva <m.angelessilvagutierrez@gmail.com>
2022-08-05 21:34:01 -0500

    movi el archivo de una carpeta a otra por un ejecutable .sh

---
+++ automover.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#creo las carpetas en reto1
+
+#mkdir foo
+#mkdir foo/dummy
+#mkdir foo/empty
+
+#mover el archivo file2.txt de la carpeta dummy a un nivel por encima empty  sin cambiarle el nombre 
+
+mv file2.txt ../empty
+#permiso a la carpeta
+# chmod -R 777 carpeta todos los permiso
+# chmod +r archivo
+# chmod +x archivo para un ejecutable
+
+
